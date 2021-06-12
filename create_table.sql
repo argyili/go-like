f3cdc0d173374d1a00d3dec962b135eb71532e8f
li-aiyi <acemonia@gmail.com>
2021-06-12 16:44:42 +0900

    Update mysql files.

--- create_table.sql
+++ create_table.sql
@@ -26,12 +26,14 @@ create table `likes` (
     `like_id` int unsigned auto_increment,
     `post_id` int unsigned not null,
     `user_id` int unsigned not null,
+    `create_at` datetime not null,
     primary key (`like_id`),
     foreign key (`post_id`) references posts(`post_id`) on update cascade,
     foreign key (`user_id`) references users(`user_id`) on update cascade
 ) engine=innodb default charset=utf8;
 
--- SELECT Count(Recipes.recipe_id)
--- FROM Recipes LEFT JOIN Reports
--- ON Recipes.recipe_id = Reports.recipe_id
--- WHERE Reports.recipe_id = NULL
+insert into users (name, create_at, update_at) values ('a', '2021-06-12', '2021-06-12');
+insert into users (name, create_at, update_at) values ('b', '2021-06-12', '2021-06-12');
+insert into posts (title, body, author_id, create_at, update_at) values ('a title', 'a content', (select user_id from users where user_id = 1), '2021-06-12', '2021-06-12');
+-- insert into likes (post_id, user_id, create_at) values ((select post_id from posts where post_id = 1),(select user_id from users where user_id = 1), '2021-06-12');
+-- insert into likes (post_id, user_id, create_at) values ((select post_id from posts where post_id = 1),(select user_id from users where user_id = 2), '2021-06-12');
